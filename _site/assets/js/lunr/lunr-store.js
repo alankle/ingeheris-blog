c8fbc7163f8811c6899805d9fa2c53b4de27a963
Alan Kleber <alankle@users.noreply.github.com>
2020-08-05 09:50:15 +0200

    Add files via upload
    full upload due to css issue.

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Bienvenidos en mi blog.",
+        "excerpt":"¡Hola a todos! You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates...","categories": ["Marketing"],
+        "tags": [],
+        "url": "http://localhost:4000/marketing/welcome-to-jekyll/",
+        "teaser": "http://localhost:4000/assets/images/bg-masthead.jpg"
+      }]
