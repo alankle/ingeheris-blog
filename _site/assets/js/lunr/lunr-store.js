caa65d6f8ce277170958122ca7c8474fdcd7086d
Alan Kleber <alan@ingeheris.com>
2020-08-05 18:17:45 +0200

    about update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Bienvenidos en mi blog.",
         "excerpt":"¡Hola a todos! You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates...","categories": ["Marketing"],
         "tags": [],
-        "url": "/marketing/welcome/",
-        "teaser": "/assets/images/bg-masthead.jpg"
+        "url": "http://localhost:4000/marketing/welcome/",
+        "teaser": "http://localhost:4000/assets/images/bg-masthead.jpg"
       }]
